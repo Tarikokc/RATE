a4da064a579203f7e837f21d2c1cb0e003e1ee46
Tarikokc <tarik.oukaci@gmail.com>
2026-04-29 15:04:27 +0200

    fix : fix css from history page

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ with app.app_context():
     init_db()
 
 DIST = os.path.join(os.path.dirname(__file__), "clientApp", "dist", "client-app", "browser")
-
+DIST = os.path.join(os.path.dirname(__file__), "clientApp", "dist", "clientApp", "browser")
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve_angular(path):
